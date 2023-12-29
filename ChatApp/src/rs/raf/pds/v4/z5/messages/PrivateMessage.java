bacda615d81ff7d3ea15623a36e9766a92f08271
danilostevanovic00 <danilostevanovic53@gmail.com>
2023-12-29 16:05:10 +0100

    Started project and added private messages func through CLI

---
+++ ChatApp/src/rs/raf/pds/v4/z5/messages/PrivateMessage.java
@@ -0,0 +1,32 @@
+package rs.raf.pds.v4.z5.messages;
+
+public class PrivateMessage {
+	String recipient;
+	String user;
+	String txt;
+	
+	protected PrivateMessage() {
+		
+	}
+
+	public PrivateMessage (String recipient, String user, String txt){
+		this.user = user;
+		this.txt = txt;
+		this.recipient= recipient;
+	}
+	
+	public String getUser() {
+		return user;
+	}
+
+	public String getTxt() {
+		return txt;
+	}
+	
+	public String getRecipient() {
+		return this.recipient;
+	}
+	
+	
+
+}
